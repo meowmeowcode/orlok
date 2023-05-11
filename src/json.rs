829569ef54a2acced191974f811f169ccd50d9b1
Anton Evdokimov <meowmeowcode@gmail.com>
2023-05-11 21:54:52 +0400

    reduce use of the `unwrap` method

--- src/json.rs
+++ src/json.rs
@@ -5,9 +5,9 @@ use std::marker::PhantomData;
 use std::pin::Pin;
 use std::str::FromStr;
 
-use anyhow::{bail, Result};
+use anyhow::{bail, Error, Result};
 use async_trait::async_trait;
-use chrono::DateTime;
+use chrono::{DateTime, FixedOffset};
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -37,8 +37,17 @@ where
         }
     }
 
-    fn load(item: Value) -> T {
-        serde_json::from_value(item).unwrap()
+    fn load(item: Value) -> Result<T> {
+        Ok(serde_json::from_value(item)?)
+    }
+
+    fn find_index(items: &Vec<Value>, filter: &F) -> Result<Option<usize>> {
+        for (index, item) in items.iter().enumerate() {
+            if matches_filter(item, filter)? {
+                return Ok(Some(index));
+            }
+        }
+        Ok(None)
     }
 }
 
@@ -52,41 +61,53 @@ where
     async fn get<'a>(&self, db: &Self::Db<'a>, filter: &F) -> Result<Option<T>> {
         let mut lock = db.data.write().await;
         let items = lock.entry(self.key.clone()).or_insert(Vec::new());
-        let item = items.iter().find(|x| matches_filter(x, filter));
-
-        match item {
-            None => Ok(None),
-            Some(i) => Ok(Some(Self::load(i.clone()))),
+        for item in items {
+            if matches_filter(item, filter)? {
+                return Ok(Some(Self::load(item.clone())?));
+            }
         }
+        Ok(None)
     }
 
     async fn get_many<'a>(&self, db: &Self::Db<'a>, query: &Query) -> Result<Vec<T>> {
         let mut lock = db.data.write().await;
         let items = lock.entry(self.key.clone()).or_insert(Vec::new());
         let mut sorted: Vec<&Value> = Vec::new();
-        let mut filtered: Box<dyn Iterator<Item = &Value>> = Box::new(items.iter());
+        let mut result: Box<dyn Iterator<Item = &Value>> = Box::new(items.iter());
 
         if let Some(filter) = &query.filter {
-            filtered = Box::new(filtered.filter(move |x| matches_filter(x, &filter.clone())))
+            result = Box::new(
+                result
+                    .try_fold(Vec::new(), move |mut acc, x| {
+                        if matches_filter(x, &filter.clone())? {
+                            acc.push(x);
+                        }
+                        Ok::<Vec<&Value>, Error>(acc)
+                    })?
+                    .into_iter(),
+            );
         }
 
         if let Some(order) = &query.order {
-            sorted.extend(filtered);
+            sorted.extend(result);
             sorted.sort_by(|x, y| {
                 vals_cmp(&extract_fields(x, order), &extract_fields(y, order), order)
             });
-            filtered = Box::new(sorted.iter().copied());
+            result = Box::new(sorted.iter().copied());
         }
 
         if let Some(offset) = query.offset {
-            filtered = Box::new(filtered.skip(offset));
+            result = Box::new(result.skip(offset));
         }
 
         if let Some(limit) = query.limit {
-            filtered = Box::new(filtered.take(limit));
+            result = Box::new(result.take(limit));
         }
 
-        Ok(filtered.map(|x| Self::load(x.clone())).collect())
+        Ok(result.try_fold(Vec::new(), |mut acc, x| {
+            acc.push(Self::load(x.clone())?);
+            Ok::<Vec<T>, Error>(acc)
+        })?)
     }
 
     async fn add<'a>(&self, db: &Self::Db<'a>, entity: &T) -> Result<()> {
@@ -101,11 +122,7 @@ where
         let mut lock = db.data.write().await;
         let items = lock.entry(self.key.clone()).or_insert(Vec::new());
 
-        while let Some((index, _)) = items
-            .iter()
-            .enumerate()
-            .find(|(_, x)| matches_filter(x, filter))
-        {
+        while let Some(index) = Self::find_index(items, filter)? {
             items.remove(index);
         }
 
@@ -117,11 +134,7 @@ where
         let items = lock.entry(self.key.clone()).or_insert(Vec::new());
         let item = serde_json::to_value(entity)?;
 
-        if let Some((index, _)) = items
-            .iter()
-            .enumerate()
-            .find(|(_, x)| matches_filter(x, filter))
-        {
+        if let Some(index) = Self::find_index(items, filter)? {
             items[index] = item;
         }
 
@@ -136,7 +149,15 @@ where
     async fn count<'a>(&self, db: &Self::Db<'a>, filter: &F) -> Result<i64> {
         let mut lock = db.data.write().await;
         let items = lock.entry(self.key.clone()).or_insert(Vec::new());
-        Ok(items.iter().filter(|x| matches_filter(x, filter)).count() as i64)
+        let mut count = 0;
+
+        for item in items {
+            if matches_filter(item, filter)? {
+                count += 1
+            }
+        }
+
+        Ok(count)
     }
 
     async fn count_all<'a>(&self, db: &Self::Db<'a>) -> Result<i64> {
@@ -150,153 +171,136 @@ where
     }
 }
 
-fn matches_filter(v: &Value, f: &F) -> bool {
-    match f {
-        F::And(filters) => matches_all_filters(v, filters),
-        F::Or(filters) => matches_any_filter(v, filters),
+fn matches_filter(v: &Value, f: &F) -> Result<bool> {
+    Ok(match f {
+        F::And(filters) => matches_all_filters(v, filters)?,
+        F::Or(filters) => matches_any_filter(v, filters)?,
         F::IsNone(field) => v[field].is_null(),
-        F::Value { field, op } => match &v[field] {
-            Value::String(s) => match op {
-                Op::StrEq(val) => *s == *val,
-                Op::StrNe(val) => *s != *val,
-                Op::StrContains(val) => s.contains(val),
-                Op::StrStartsWith(val) => s.starts_with(val),
-                Op::StrEndsWith(val) => s.ends_with(val),
-                Op::StrIn(val) => val.contains(s),
-                Op::DateTimeEq(val) => DateTime::parse_from_rfc3339(&s).unwrap() == *val,
-                Op::DateTimeNe(val) => DateTime::parse_from_rfc3339(&s).unwrap() != *val,
-                Op::DateTimeLt(val) => DateTime::parse_from_rfc3339(&s).unwrap() < *val,
-                Op::DateTimeGt(val) => DateTime::parse_from_rfc3339(&s).unwrap() > *val,
-                Op::DateTimeLte(val) => DateTime::parse_from_rfc3339(&s).unwrap() <= *val,
-                Op::DateTimeGte(val) => DateTime::parse_from_rfc3339(&s).unwrap() >= *val,
-                Op::DecimalEq(val) => Decimal::from_str(s).unwrap() == *val,
-                Op::DecimalNe(val) => Decimal::from_str(s).unwrap() != *val,
-                Op::DecimalLt(val) => Decimal::from_str(s).unwrap() < *val,
-                Op::DecimalGt(val) => Decimal::from_str(s).unwrap() > *val,
-                Op::DecimalLte(val) => Decimal::from_str(s).unwrap() <= *val,
-                Op::DecimalGte(val) => Decimal::from_str(s).unwrap() >= *val,
-                Op::UuidEq(val) => Uuid::parse_str(s).unwrap() == *val,
-                Op::UuidNe(val) => Uuid::parse_str(s).unwrap() != *val,
-                Op::UuidIn(val) => val.contains(&Uuid::parse_str(s).unwrap()),
-                _ => false,
-            },
-            Value::Number(n) => match op {
-                Op::IntEq(val) => {
-                    if let Some(n) = n.as_i64() {
-                        n == *val
-                    } else {
-                        false
-                    }
+        F::Value { field, op } => {
+            if let Some(val) = v.get(field) {
+                if val.is_null() {
+                    return Ok(false);
                 }
-                Op::IntNe(val) => {
-                    if let Some(n) = n.as_i64() {
-                        n != *val
-                    } else {
-                        false
+                match op {
+                    Op::StrEq(arg) => extract_string(val)? == arg,
+                    Op::StrNe(arg) => extract_string(val)? != arg,
+                    Op::StrContains(arg) => extract_string(val)?.contains(arg),
+                    Op::StrStartsWith(arg) => extract_string(val)?.starts_with(arg),
+                    Op::StrEndsWith(arg) => extract_string(val)?.ends_with(arg),
+                    Op::StrIn(arg) => arg.contains(extract_string(val)?),
+                    Op::DateTimeEq(arg) => extract_date_time(val)? == *arg,
+                    Op::DateTimeNe(arg) => extract_date_time(val)? != *arg,
+                    Op::DateTimeLt(arg) => extract_date_time(val)? < *arg,
+                    Op::DateTimeGt(arg) => extract_date_time(val)? > *arg,
+                    Op::DateTimeLte(arg) => extract_date_time(val)? <= *arg,
+                    Op::DateTimeGte(arg) => extract_date_time(val)? >= *arg,
+                    Op::DecimalEq(arg) => extract_decimal(val)? == *arg,
+                    Op::DecimalNe(arg) => extract_decimal(val)? != *arg,
+                    Op::DecimalLt(arg) => extract_decimal(val)? < *arg,
+                    Op::DecimalGt(arg) => extract_decimal(val)? > *arg,
+                    Op::DecimalLte(arg) => extract_decimal(val)? <= *arg,
+                    Op::DecimalGte(arg) => extract_decimal(val)? >= *arg,
+                    Op::UuidEq(arg) => extract_uuid(val)? == *arg,
+                    Op::UuidNe(arg) => extract_uuid(val)? != *arg,
+                    Op::UuidIn(arg) => arg.contains(&extract_uuid(val)?),
+                    Op::IntEq(arg) => extract_int(val)? == *arg,
+                    Op::IntNe(arg) => extract_int(val)? != *arg,
+                    Op::IntLt(arg) => extract_int(val)? < *arg,
+                    Op::IntGt(arg) => extract_int(val)? > *arg,
+                    Op::IntLte(arg) => extract_int(val)? <= *arg,
+                    Op::IntGte(arg) => extract_int(val)? >= *arg,
+                    Op::IntBetween(arg1, arg2) => {
+                        let n = extract_int(val)?;
+                        *arg1 <= n && n <= *arg2
                     }
+                    Op::IntIn(arg) => arg.contains(&extract_int(val)?),
+                    Op::FloatEq(arg) => extract_float(val)? == *arg,
+                    Op::FloatNe(arg) => extract_float(val)? != *arg,
+                    Op::FloatLt(arg) => extract_float(val)? < *arg,
+                    Op::FloatGt(arg) => extract_float(val)? > *arg,
+                    Op::FloatLte(arg) => extract_float(val)? <= *arg,
+                    Op::FloatGte(arg) => extract_float(val)? >= *arg,
+                    Op::BoolEq(arg) => extract_bool(val)? == *arg,
+                    Op::BoolNe(arg) => extract_bool(val)? != *arg,
                 }
-                Op::IntLt(val) => {
-                    if let Some(n) = n.as_i64() {
-                        n < *val
-                    } else {
-                        false
-                    }
-                }
-                Op::IntGt(val) => {
-                    if let Some(n) = n.as_i64() {
-                        n > *val
-                    } else {
-                        false
-                    }
-                }
-                Op::IntLte(val) => {
-                    if let Some(n) = n.as_i64() {
-                        n <= *val
-                    } else {
-                        false
-                    }
-                }
-                Op::IntGte(val) => {
-                    if let Some(n) = n.as_i64() {
-                        n >= *val
-                    } else {
-                        false
-                    }
-                }
-                Op::IntBetween(val1, val2) => {
-                    if let Some(n) = n.as_i64() {
-                        *val1 <= n && n <= *val2
-                    } else {
-                        false
-                    }
-                }
-                Op::IntIn(val) => {
-                    if let Some(n) = n.as_i64() {
-                        val.contains(&n)
-                    } else {
-                        false
-                    }
-                }
-                Op::FloatEq(val) => {
-                    if let Some(n) = n.as_f64() {
-                        n == *val
-                    } else {
-                        false
-                    }
-                }
-                Op::FloatNe(val) => {
-                    if let Some(n) = n.as_f64() {
-                        n != *val
-                    } else {
-                        false
-                    }
-                }
-                Op::FloatLt(val) => {
-                    if let Some(n) = n.as_f64() {
-                        n < *val
-                    } else {
-                        false
-                    }
-                }
-                Op::FloatGt(val) => {
-                    if let Some(n) = n.as_f64() {
-                        n > *val
-                    } else {
-                        false
-                    }
-                }
-                Op::FloatLte(val) => {
-                    if let Some(n) = n.as_f64() {
-                        n <= *val
-                    } else {
-                        false
-                    }
-                }
-                Op::FloatGte(val) => {
-                    if let Some(n) = n.as_f64() {
-                        n >= *val
-                    } else {
-                        false
-                    }
-                }
-                _ => false,
-            },
-            Value::Bool(b) => match op {
-                Op::BoolEq(val) => b == val,
-                Op::BoolNe(val) => b != val,
-                _ => false,
-            },
-            _ => false,
-        },
+            } else {
+                bail!("Unknown field {}", field)
+            }
+        }
+    })
+}
+
+fn matches_all_filters(v: &Value, filters: &[F]) -> Result<bool> {
+    for f in filters {
+        if !matches_filter(&v, f)? {
+            return Ok(false);
+        }
+    }
+    Ok(true)
+}
+
+fn matches_any_filter(v: &Value, filters: &[F]) -> Result<bool> {
+    for f in filters {
+        if matches_filter(&v, f)? {
+            return Ok(true);
+        }
+    }
+    Ok(false)
+}
+
+fn extract_string(v: &Value) -> Result<&String> {
+    if let Value::String(s) = v {
+        Ok(s)
+    } else {
+        bail!("{:?} is not a string", v)
     }
 }
 
-fn matches_all_filters(v: &Value, filters: &[F]) -> bool {
-    filters.iter().all(|f| matches_filter(&v, f))
+fn extract_date_time(v: &Value) -> Result<DateTime<FixedOffset>> {
+    let s = extract_string(v)?;
+    Ok(DateTime::parse_from_rfc3339(&s)?)
 }
 
-fn matches_any_filter(v: &Value, filters: &[F]) -> bool {
-    filters.iter().any(|f| matches_filter(&v, f))
+fn extract_decimal(v: &Value) -> Result<Decimal> {
+    let s = extract_string(v)?;
+    Ok(Decimal::from_str(s)?)
+}
+
+fn extract_uuid(v: &Value) -> Result<Uuid> {
+    let s = extract_string(v)?;
+    Ok(Uuid::parse_str(s)?)
+}
+
+fn extract_int(v: &Value) -> Result<i64> {
+    if let Value::Number(n) = v {
+        if let Some(n) = n.as_i64() {
+            Ok(n)
+        } else {
+            bail!("Cannot convert {:?} to an f64", n);
+        }
+    } else {
+        bail!("{:?} is not a number", v);
+    }
+}
+
+fn extract_float(v: &Value) -> Result<f64> {
+    if let Value::Number(n) = v {
+        if let Some(n) = n.as_f64() {
+            Ok(n)
+        } else {
+            bail!("Cannot convert {:?} to f64", n);
+        }
+    } else {
+        bail!("{:?} is not a number", v);
+    }
+}
+
+fn extract_bool(v: &Value) -> Result<bool> {
+    if let Value::Bool(b) = v {
+        Ok(*b)
+    } else {
+        bail!("{:?} is not a bool", v);
+    }
 }
 
 fn extract_fields<'a>(v: &'a Value, order: &Vec<Order>) -> Vec<&'a Value> {
@@ -312,9 +316,15 @@ fn extract_fields<'a>(v: &'a Value, order: &Vec<Order>) -> Vec<&'a Value> {
 
 fn val_cmp(x: &Value, y: &Value, order: &Order) -> Ordering {
     let result = match (x, y) {
-        (Value::String(s1), Value::String(s2)) => s1.as_str().cmp(&s2.as_str()),
-        (Value::Number(n1), Value::Number(n2)) => n1.as_i64().unwrap().cmp(&n2.as_i64().unwrap()),
-        _ => todo!(),
+        (Value::Null, Value::Null) => Ordering::Equal,
+        (Value::Null, _) => Ordering::Less,
+        (_, Value::Null) => Ordering::Greater,
+        (Value::String(s1), Value::String(s2)) => s1.cmp(&s2),
+        (Value::Number(n1), Value::Number(n2)) => {
+            n1.as_f64().unwrap().total_cmp(&n2.as_f64().unwrap())
+        }
+        (Value::Bool(b1), Value::Bool(b2)) => b1.cmp(b2),
+        _ => Ordering::Equal,
     };
 
     match order {
